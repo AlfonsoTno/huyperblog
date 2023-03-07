7f16a472838a138d8d2d931adb9c74d938048529
Alfonso Tolentino <alfonso.tno@gmail.com>
2023-03-06 19:59:46 -0600

    módulo 2 y 3 de Python

--- python/main.py
+++ python/main.py
@@ -1,1 +1,18 @@
 print('Hola te encuentras en un nuevo curso de Código facilito.')
+lista = ['String',10,15.6, True] # list()
+
+print(lista)
+
+lista_strings = ["Eduardo", "Código", "Curso Pyhton"]
+
+lista_enteros = [-19, 0, 10 ,200]
+
+lista_floats = [19.5, -20.5, 80.99, 100.00]
+
+lista_booleanos = [True, True, (2 < 10), (4>3 and 10 != 11)]
+
+
+
+tupla = ('String', 10, 15.4, [1, 2, 3], (4, 5, 6))
+
+print(tupla)
