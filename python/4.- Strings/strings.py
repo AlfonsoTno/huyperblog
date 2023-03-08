e83f79276270c720463d1f5d3b7b8871239f2a5b
Alfonso Tolentino <alfonso.tno@gmail.com>
2023-03-08 07:13:21 -0600

    Modulo 4

--- python/4.- Strings/strings.py
+++ python/4.- Strings/strings.py
@@ -1,5 +1,16 @@
+"""
 lenguajes = 'Python-Ruby-Java-Rust-C++-C'
 
-listado_lenguajes = lenguajes.split('-')
+listado_lenguajes = lenguajes.split('-') # lo convierte en listado
 
 print(listado_lenguajes)
+"""
+
+lenguajes = ['Python','Ruby','Java','Rust']
+
+
+string_lenguajes = ' '.join(lenguajes) # lo convierte en string 
+
+print(string_lenguajes)
+print(type(string_lenguajes))
+
