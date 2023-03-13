eef24f9ebcaab4bad83b83ee3c69f0efa6791f23
Alfonso Tolentino <alfonso.tno@gmail.com>
2023-03-13 00:11:00 -0600

    numpy y pandas

---
+++ python/Curso análisis de datos con Paython/env/main.py
@@ -0,0 +1,16 @@
+def suma(a: int, b: int) -> int:
+    """Permite sumar 2 números enteros.
+
+    Args:
+        a (int): N1
+        b (int): N2
+
+    Returns:
+        int: La suma de a + b
+    """
+    result = a + b	
+    return result
+
+print(suma(10, 20))
+print(suma(20, 30))
+print(suma(40, 50))
---
+++ python/Curso análisis de datos con Paython/env/operaciones.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+def resta(a: int, b: int) -> int:
+    """Permite restar 2 números neteros. """
+    return a - b
+     
+resultado = resta(10, 20)
+def suma(a: int, b: int) -> int:
+    """Permite restar 2 números neteros. """
+    return a + b
+    
+resultado = suma(10, 20)
+print(resultado)
